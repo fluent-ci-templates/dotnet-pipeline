073dc86e4451b088eab473a7d4b76e8aa847903f
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-08 15:22:30 +0000

    feat: handle pipeline arguments

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   test,
-} from "https://pkg.fluentci.io/dotnet_pipeline@v0.3.1/mod.ts";
+} from "https://pkg.fluentci.io/dotnet_pipeline@v0.4.0/mod.ts";
 
 await test();
 await build();
