f8dcf53dbe47e3b438e2b6daaade1ea717f7c918
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-12-17 17:39:53 +0000

    update README.md

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   test,
-} from "https://pkg.fluentci.io/dotnet_pipeline@v0.4.0/mod.ts";
+} from "https://pkg.fluentci.io/dotnet_pipeline@v0.5.0/mod.ts";
 
 await test();
 await build();
