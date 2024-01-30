ecb3ac1f7f2ad8eb69be4e803f9152d444202428
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-30 08:17:34 +0000

    fix issue with dagger connect

--- src/dagger/jobs.ts
+++ src/dagger/jobs.ts
@@ -1,5 +1,4 @@
-import { Client, Directory } from "../../sdk/client.gen.ts";
-import { connect } from "../../sdk/connect.ts";
+import { Directory, dag } from "../../sdk/client.gen.ts";
 import { getDirectory } from "./lib.ts";
 
 export enum Job {
@@ -16,32 +15,29 @@ export const exclude = [".git", ".fluentci", "bin"];
  * @returns {Promise<string>}
  */
 export async function test(src: Directory | string = "."): Promise<string> {
-  let result = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.test)
-      .container()
-      .from("alpine")
-      .withExec(["apk", "add", "bash", "wget"])
-      .withExec([
-        "wget",
-        "https://dot.net/v1/dotnet-install.sh",
-        "-O",
-        "dotnet-install.sh",
-      ])
-      .withExec(["chmod", "+x", "dotnet-install.sh"])
-      .withExec(["./dotnet-install.sh"])
-      .withExec(["apk", "add", "build-base", "icu-libs"])
-      .withEnvVariable("DOTNET_ROOT", "/root/.dotnet")
-      .withEnvVariable("PATH", "/root/.dotnet:$PATH", { expand: true })
-      .withExec(["dotnet", "--info"])
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["dotnet", "test"]);
+  const context = await getDirectory(dag, src);
+  const ctr = dag
+    .pipeline(Job.test)
+    .container()
+    .from("alpine")
+    .withExec(["apk", "add", "bash", "wget"])
+    .withExec([
+      "wget",
+      "https://dot.net/v1/dotnet-install.sh",
+      "-O",
+      "dotnet-install.sh",
+    ])
+    .withExec(["chmod", "+x", "dotnet-install.sh"])
+    .withExec(["./dotnet-install.sh"])
+    .withExec(["apk", "add", "build-base", "icu-libs"])
+    .withEnvVariable("DOTNET_ROOT", "/root/.dotnet")
+    .withEnvVariable("PATH", "/root/.dotnet:$PATH", { expand: true })
+    .withExec(["dotnet", "--info"])
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["dotnet", "test"]);
 
-    result = await ctr.stdout();
-  });
+  const result = await ctr.stdout();
   return result;
 }
 
@@ -54,35 +50,32 @@ export async function test(src: Directory | string = "."): Promise<string> {
 export async function build(
   src: Directory | string | undefined = "."
 ): Promise<Directory | string> {
-  let id = "";
-  await connect(async (client: Client) => {
-    const context = getDirectory(client, src);
-    const ctr = client
-      .pipeline(Job.build)
-      .container()
-      .from("alpine")
-      .withExec(["apk", "add", "bash", "wget"])
-      .withExec([
-        "wget",
-        "https://dot.net/v1/dotnet-install.sh",
-        "-O",
-        "dotnet-install.sh",
-      ])
-      .withExec(["chmod", "+x", "dotnet-install.sh"])
-      .withExec(["./dotnet-install.sh"])
-      .withExec(["apk", "add", "build-base", "icu-libs"])
-      .withEnvVariable("DOTNET_ROOT", "/root/.dotnet")
-      .withEnvVariable("PATH", "/root/.dotnet:$PATH", { expand: true })
-      .withExec(["dotnet", "--info"])
-      .withMountedCache("/app/bin", client.cacheVolume("dotnet-bin"))
-      .withDirectory("/app", context, { exclude })
-      .withWorkdir("/app")
-      .withExec(["dotnet", "build"])
-      .withExec(["cp", "-r", "bin", "/dotnet-bin"]);
+  const context = await getDirectory(dag, src);
+  const ctr = dag
+    .pipeline(Job.build)
+    .container()
+    .from("alpine")
+    .withExec(["apk", "add", "bash", "wget"])
+    .withExec([
+      "wget",
+      "https://dot.net/v1/dotnet-install.sh",
+      "-O",
+      "dotnet-install.sh",
+    ])
+    .withExec(["chmod", "+x", "dotnet-install.sh"])
+    .withExec(["./dotnet-install.sh"])
+    .withExec(["apk", "add", "build-base", "icu-libs"])
+    .withEnvVariable("DOTNET_ROOT", "/root/.dotnet")
+    .withEnvVariable("PATH", "/root/.dotnet:$PATH", { expand: true })
+    .withExec(["dotnet", "--info"])
+    .withMountedCache("/app/bin", dag.cacheVolume("dotnet-bin"))
+    .withDirectory("/app", context, { exclude })
+    .withWorkdir("/app")
+    .withExec(["dotnet", "build"])
+    .withExec(["cp", "-r", "bin", "/dotnet-bin"]);
 
-    await ctr.stdout();
-    id = await ctr.directory("/dotnet-bin").id();
-  });
+  await ctr.stdout();
+  const id = await ctr.directory("/dotnet-bin").id();
   return id;
 }
 
