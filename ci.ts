7e14b4cc73f5a52670bc58c4b0eb46951fe575cc
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-30 08:17:34 +0000

    fix issue with dagger connect

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   test,
-} from "https://pkg.fluentci.io/dotnet_pipeline@v0.5.0/mod.ts";
+} from "https://pkg.fluentci.io/dotnet_pipeline@v0.5.1/mod.ts";
 
 await test();
 await build();
--- example/.fluentci/src/dagger/lib.ts
+++ example/.fluentci/src/dagger/lib.ts
@@ -1,37 +1,18 @@
-import Client, {
-  Directory,
-  DirectoryID,
-  Secret,
-  SecretID,
-} from "../../deps.ts";
+import { Directory, DirectoryID } from "../../deps.ts";
+import { Client } from "../../sdk/client.gen.ts";
 
-export const getDirectory = (
+export const getDirectory = async (
   client: Client,
   src: string | Directory | undefined = "."
 ) => {
-  if (typeof src === "string" && src.startsWith("core.Directory")) {
-    return client.directory({
-      id: src as DirectoryID,
-    });
+  if (typeof src === "string") {
+    try {
+      const directory = client.loadDirectoryFromID(src as DirectoryID);
+      await directory.id();
+      return directory;
+    } catch (_) {
+      return client.host().directory(src);
+    }
   }
   return src instanceof Directory ? src : client.host().directory(src);
-};
-
-export const getDenoDeployToken = (client: Client, token?: string | Secret) => {
-  if (Deno.env.get("DENO_DEPLOY_TOKEN")) {
-    return client.setSecret(
-      "DENO_DEPLOY_TOKEN",
-      Deno.env.get("DENO_DEPLOY_TOKEN")!
-    );
-  }
-  if (token && typeof token === "string") {
-    if (token.startsWith("core.Secret")) {
-      return client.loadSecretFromID(token as SecretID);
-    }
-    return client.setSecret("DENO_DEPLOY_TOKEN", token);
-  }
-  if (token && token instanceof Secret) {
-    return token;
-  }
-  return undefined;
 };
