8928cd33cb9b0725aea54f37d88a41eb5aeb7d99
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-17 19:53:32 +0000

    ci: use dagger v0.11.0
    ci: fix ci.yml
    
    fix example
    
    fix example

--- plugin/src/helpers.rs
+++ plugin/src/helpers.rs
@@ -7,10 +7,12 @@ pub fn setup_dotnet() -> Result<String, Error> {
     let home = dag().get_env("HOME")?;
     let dotnet_home = format!("{}/.dotnet", home);
     let path = dag().get_env("PATH")?;
+
     dag().set_envs(vec![
         ("DOTNET_ROOT".into(), dotnet_home.clone()),
-        ("PATH".into(), format!("{}/.dotnet:{}", dotnet_home, path)),
+        ("PATH".into(), format!("{}/.dotnet:{}", home, path)),
     ])?;
+
     let stdout = dag()
         .pkgx()?
         .with_exec(vec![
